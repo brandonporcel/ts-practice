13251d991419d0b59ed2ae49cafd8060cbbf816c
brandonn <brandon7.7porcel@gmail.com>
2023-04-18 18:24:45 -0300

    proyecto terminado.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,63 +3,141 @@ import {
 	FormControl,
 	FormLabel,
 	Input,
+	Link,
 	Select,
 	Stack,
 } from '@chakra-ui/react';
-import React, { useRef, useState } from 'react';
-interface IUser {
-	link: string;
-	type: string;
-	id?: Date;
-}
-export default function Home() {
-	const initialState = { link: '', type: 'yt' };
-	const [form, setForm] = useState<IUser>(initialState);
-	const [cosos, setCosos] = useState([
-		{
-			link: 'sdasd',
-		},
-	]);
-	// const ytIframe = useRef(null);
-	// console.log(ytIframe.current?.allowfullscreen, 'a');
 
-	const handleChange = (e: any) => {
+import React, { useEffect, useState } from 'react';
+import Papa from 'papaparse';
+interface VideoType {
+	type: string;
+	id?: number;
+	url: string;
+	title: string;
+}
+type Parsero = {
+	data: Array<VideoType>;
+};
+export default function Home() {
+	const initialState = { url: '', type: '', title: '' };
+	const [form, setForm] = useState<VideoType>(initialState);
+
+	const [cosos, setCosos] = useState<VideoType[]>([]);
+	const [loading, setLoading] = useState(true);
+
+	// api
+	useEffect(() => {
+		const getData = async () => {
+			const data = await fetch(
+				'https://docs.google.com/spreadsheets/d/e/2PACX-1vQOjWkR2tEt46kpQ4kIH5qVuPEus4v2X3QQQfMpy9K3hQGv_Brg0pQEY71mK7sxiOnN9FmWLC6-rIF-/pub?output=csv'
+			);
+			const resp = await data.text();
+			const parsed: Parsero = await new Promise((resolve, reject) => {
+				Papa.parse(resp, { header: true, complete: resolve, error: reject });
+			});
+
+			setCosos(parsed.data);
+			setLoading(false);
+		};
+		getData();
+	}, []);
+	const [galleryVideo, setGalleryVideo] = useState(0);
+
+	const prevVid = () => {
+		galleryVideo === 0
+			? setGalleryVideo(cosos?.length - 1)
+			: setGalleryVideo((galleryVideo) => galleryVideo - 1);
+	};
+	const nextVid = () => {
+		galleryVideo === cosos?.length - 1
+			? setGalleryVideo(0)
+			: setGalleryVideo((galleryVideo) => galleryVideo + 1);
+	};
+
+	const handleChange = (
+		e:
+			| React.ChangeEvent<HTMLInputElement>
+			| React.ChangeEvent<HTMLSelectElement>
+	) => {
 		setForm({ ...form, [e.target.name]: e.target.value });
 	};
+
 	const addNew = (e: React.FormEvent<HTMLFormElement>) => {
 		e.preventDefault();
-		setCosos([...cosos, form]);
+		setCosos([...cosos, { ...form, id: cosos?.length }]);
 	};
 	return (
 		<>
-			<Stack width="100" background={'gray'} height="100vh">
-				<iframe
-					width="100%"
-					height="100%"
-					src="https://www.youtube.com/embed/EgDQMPyOlbM"
-					title="YouTube video playerrrrr"
-					style={{ border: '0' }}
-					allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-					// data-allowfullscreen
-				></iframe>
-
-				{cosos.map((el) => {
-					return <p>{el.link}</p>;
-				})}
+			<Stack width={'100%'} height={'100vh'} overflowX={'hidden'}>
+				{loading ? (
+					<p>cargandro...</p>
+				) : cosos[galleryVideo]?.type === 'img' ? (
+					<Stack
+						className="appear marquee"
+						height={'100%'}
+						backgroundRepeat={'no-repeat'}
+						backgroundSize={'cover'}
+						backgroundPosition={'center'}
+						position={'relative'}
+						style={{ backgroundImage: `url(${cosos[galleryVideo]?.url})` }}
+					>
+						<Stack
+							className="marqueeSon"
+							position={'absolute'}
+							style={{ backgroundImage: `url(${cosos[galleryVideo]?.url})` }}
+						></Stack>
+						<small style={{ color: 'white' }}>
+							{cosos[galleryVideo].title}
+						</small>
+					</Stack>
+				) : (
+					<iframe
+						width="100%"
+						height="100%"
+						src={`https://www.youtube.com/embed/${cosos[galleryVideo]?.url}`}
+						title="YouTube video playerrrrr"
+						style={{ border: '0' }}
+						allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+					></iframe>
+				)}
 			</Stack>
-			<form onSubmit={(e) => addNew(e)}>
-				<FormControl>
-					<FormLabel>Link</FormLabel>
-					<Input
-						type="text"
-						placeholder="https"
-						name="link"
-						required
-						value={form.link}
-						onChange={(e) => handleChange(e)}
-					/>
-					<FormControl>
-						<FormLabel>Type</FormLabel>
+
+			<Stack p="4">
+				<Stack>
+					<Button mt={4} colorScheme="teal" type="button" onClick={prevVid}>
+						⬅
+					</Button>
+					<Button mt={4} colorScheme="teal" type="button" onClick={nextVid}>
+						➡
+					</Button>
+				</Stack>
+
+				<form onSubmit={(e) => addNew(e)}>
+					<FormControl marginTop={5}>
+						<FormLabel fontSize={20}>Title</FormLabel>
+						<Input
+							type="text"
+							placeholder="Messi World Cup Final"
+							name="title"
+							required
+							value={form.title}
+							onChange={(e) => handleChange(e)}
+						/>
+					</FormControl>
+					<FormControl marginTop={5}>
+						<FormLabel fontSize={20}>Url</FormLabel>
+						<Input
+							type="text"
+							placeholder="TWOEYOWkGSs"
+							name="url"
+							required
+							value={form.url}
+							onChange={(e) => handleChange(e)}
+						/>
+					</FormControl>
+					<FormControl marginTop={5}>
+						<FormLabel fontSize={20}>Type</FormLabel>
 						<Select
 							required
 							placeholder="Tipo link"
@@ -67,15 +145,24 @@ export default function Home() {
 							name="type"
 							onChange={(e) => handleChange(e)}
 						>
-							<option value="tw">tw</option>
-							<option value="yt">yt</option>
+							<option value="yt">youtube</option>
+							<option value="img">image</option>
 						</Select>
 					</FormControl>
 					<Button mt={4} colorScheme="teal" type="submit">
 						Submit
 					</Button>
-				</FormControl>
-			</form>
+					<Stack>
+						<a
+							className="grad"
+							href="https://docs.google.com/spreadsheets/d/1QI9BNWenfF4UeQYPa-KDhJp6epI1xB0_Io-Spq6pIwY/edit?usp=sharing"
+							target="_blank"
+						>
+							Excel
+						</a>
+					</Stack>
+				</form>
+			</Stack>
 		</>
 	);
 }
